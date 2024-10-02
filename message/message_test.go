731ecb5b65e0b02f84aef72ea78cfaa9bbbf18ac
marchuanv <marchuanv@gmail.com>
2024-10-02 13:11:44 +0200

    message struct built

--- message/message_test.go
+++ message/message_test.go
@@ -4,26 +4,34 @@ import (
 	"testing"
 )
 
-func testMsgCtorWithoutArgs(msgText string, t *testing.T) *Message {
+func testMsgCtorWithArgs(msgText string, t *testing.T) (*Message, error) {
 	msgPtr, err := New(msgText)
 	if err != nil {
 		t.Errorf("CtorError: failed to create a message with errors")
 		t.Logf("%s", err)
+		return nil, err
+	} else {
+		return msgPtr, nil
 	}
-	return msgPtr
 }
 
-func testUUIDEquality(t *testing.T) {
-	msgRef1 := testMsgCtorWithoutArgs("Hello World", t)
-	msgRef2 := testMsgCtorWithoutArgs("Hello World", t)
-	msgRef3 := testMsgCtorWithoutArgs("Hello John", t)
-	if msgRef1 != msgRef2 {
-		t.Errorf("CtorError: expected message pointers to be the same")
+func TestMsgEquality(t *testing.T) {
+	msgRef1, err1 := testMsgCtorWithArgs("Hello World", t)
+	msgRef2, err2 := testMsgCtorWithArgs("Hello World", t)
+	msgRef3, err3 := testMsgCtorWithArgs("Hello John", t)
+	if err1 == nil && err2 == nil {
+		if msgRef1 != msgRef2 {
+			t.Errorf("CtorError: expected message pointers to be the same")
+		}
 	}
-	if msgRef1 == msgRef3 {
-		t.Errorf("CtorError: expected message pointers to NOT be the same")
+	if err1 == nil && err3 == nil {
+		if msgRef3 == msgRef1 {
+			t.Errorf("CtorError: expected message pointers to NOT be the same")
+		}
 	}
-}
-func TestMsgCtor(t *testing.T) {
-	testUUIDEquality(t)
+	if err1 == nil && err2 == nil {
+		if msgRef3 == msgRef2 {
+			t.Errorf("CtorError: expected message pointers to NOT be the same")
+		}
+	}
 }
