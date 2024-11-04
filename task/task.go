9dd9cfe31c94cafa756d660f7727c61ce6e1aab9
marchuanv <marchuanv@gmail.com>
2024-11-04 18:24:57 +0200

    refactor

--- task/task.go
+++ task/task.go
@@ -6,73 +6,102 @@ import (
 )
 
 type task struct {
-	doFuncRef     reflect.Value
-	errorFuncRef  reflect.Value
-	errVal        reflect.Value
-	errorParamVal reflect.Value
-	resultsVal    reflect.Value
+	doFuncRef       reflect.Value
+	errorFuncRef    reflect.Value
+	errVal          reflect.Value
+	errorParamVal   reflect.Value
+	resultsVal      reflect.Value
+	hasErrors       bool
+	isErrorsHandled bool
+	isChild         bool
 }
 
-var errorStack = taskStack{}
-var emptyParameters = []reflect.Value{}
+var tskStack = taskStack{}
+var taskCount = 0
 
-func Do[T any, T2 any](doFunc func() (T, error), errorFunc ...func(err error, errorParam T2) T2) T {
+func Do[T1 any, T2 any](doFunc func() (T1, error), errorFunc ...func(err error, errorParam T2) T2) T1 {
 	var err error
 	var errorParam T2
-	var results T
+	var results T1
 	doFuncRef := reflect.Indirect(reflect.ValueOf(doFunc))
 	errVal := reflect.Indirect(reflect.ValueOf(err))
 	errorParamVal := reflect.Indirect(reflect.ValueOf(errorParam))
 	resultsVal := reflect.Indirect(reflect.ValueOf(results))
-
 	var errorFuncRef reflect.Value
 	if len(errorFunc) > 0 {
 		errorFuncRef = reflect.Indirect(reflect.ValueOf(errorFunc[0]))
 	}
-
 	tsk := &task{
 		doFuncRef,
 		errorFuncRef,
 		errVal,
 		errorParamVal,
 		resultsVal,
+		false,
+		false,
+		false,
 	}
-
-	if len(errorStack) == 0 {
-		values := tsk.doFuncRef.Call(emptyParameters)
-		tsk.resultsVal = reflect.Indirect(reflect.ValueOf(values[0]))
-		tsk.errVal = reflect.Indirect(reflect.ValueOf(values[1]))
-		if tsk.errVal.IsZero() {
-			results = tsk.resultsVal.Interface().(T)
-		} else {
-			err = tsk.resultsVal.Interface().(error)
-			if !tsk.errorFuncRef.IsZero() {
-				callErrorFunction(tsk)
-			}
-			errorStack.Push(tsk)
-		}
+	taskCount += 1
+	if taskCount > 1 {
+		tsk.isChild = true
 	} else {
-		var tskErr *task
-		errorStack, tskErr = errorStack.Pop()
-		if !tsk.errorFuncRef.IsZero() {
-			isErrFuncAssign := tsk.errorFuncRef.Type().AssignableTo(tskErr.errorFuncRef.Type())
-			isErrAssign := tsk.errVal.Type().AssignableTo(tskErr.errVal.Type())
+		tsk.isChild = false
+	}
+	callDoFunction(tsk)
+	taskCount -= 1
+	tskStack.Push(tsk)
+	if tsk.isChild {
+		tsk = nil
+		return tskResult[T1](tsk)
+	}
+	for tskStack.Len() > 0 {
+		popTsk := tskStack.Pop()
+		if popTsk.hasErrors && !popTsk.errorFuncRef.IsZero() {
+			isErrFuncAssign := tsk.errorFuncRef.Type().AssignableTo(popTsk.errorFuncRef.Type())
+			isErrAssign := tsk.errVal.Type().AssignableTo(popTsk.errVal.Type())
 			if isErrFuncAssign && isErrAssign {
-				tsk.errVal = tskErr.errVal
-				tsk.errorParamVal = tskErr.errorParamVal
+				tsk.errVal = popTsk.errVal
+				tsk.errorParamVal = popTsk.errorParamVal
 				callErrorFunction(tsk)
+				tsk.isErrorsHandled = true
 			} else {
-				err = errors.New("one or more error function in the task chain does not have the correct function signatures")
-				panic(err)
+				unassignableErr := errors.New("one or more error function in the task chain does not have the correct function signatures")
+				panic(unassignableErr)
 			}
 		}
-		errorStack.Push(tskErr)
 	}
-	return results
+	if tsk.hasErrors && !tsk.isErrorsHandled {
+		tskErr := tskError(tsk)
+		panic(tskErr)
+	}
+	return tskResult[T1](tsk)
 }
+
+func callDoFunction(tsk *task) {
+	out := tsk.doFuncRef.Call([]reflect.Value{})
+	tsk.resultsVal = out[0]
+	tsk.errVal = out[1]
+	if !tsk.errVal.IsZero() {
+		tsk.hasErrors = true
+		tsk.isErrorsHandled = false
+	}
+	out = nil
+}
+
 func callErrorFunction(tsk *task) {
-	values := []reflect.Value{tsk.errVal, tsk.errorParamVal}
-	values = tsk.errorFuncRef.Call(values)
-	tsk.errorParamVal = reflect.Indirect(reflect.ValueOf(values[0]))
-	values = nil
+	out := tsk.errorFuncRef.Call([]reflect.Value{tsk.errVal, tsk.errorParamVal})
+	tsk.errorParamVal = out[0]
+	out = nil
+}
+
+func tskError(tsk *task) error {
+	return tsk.errVal.Interface().(error)
+}
+
+func tskErrorParam[T any](tsk *task) T {
+	return tsk.errorParamVal.Interface().(T)
+}
+
+func tskResult[T any](tsk *task) T {
+	return tsk.resultsVal.Interface().(T)
 }
--- task/tskstack.go
+++ task/tskstack.go
@@ -2,26 +2,40 @@ package task
 
 import "sync"
 
-type taskStack []*task
-
-var mu = sync.Mutex{}
-
-func (s taskStack) Push(tsk *task) taskStack {
-	defer mu.Unlock()
-	mu.Lock()
-	return append(s, tsk)
+type taskStack struct {
+	tasks  []*task
+	mu     sync.Mutex
+	locked bool
 }
 
-func (s taskStack) Pop() (taskStack, *task) {
-	defer mu.Unlock()
-	mu.Lock()
-	l := len(s)
-	return s[:l-1], s[l-1]
+func (s *taskStack) Push(tsk *task) {
+	s.tasks = append(s.tasks, tsk)
 }
 
-func (s taskStack) Peek() *task {
-	defer mu.Unlock()
-	mu.Lock()
-	l := len(s)
-	return s[l-1]
+func (s *taskStack) Pop() *task {
+	l := len(s.tasks)
+	tsk := s.tasks[l-1]
+	s.tasks = s.tasks[:l-1]
+	return tsk
+}
+
+func (s *taskStack) Peek() *task {
+	l := len(s.tasks)
+	return s.tasks[l-1]
+}
+
+func (s *taskStack) Lock() {
+	s.mu.Lock()
+}
+
+func (s *taskStack) Unlock() {
+	s.mu.Unlock()
+}
+
+func (s *taskStack) IsLocked() bool {
+	return !s.locked
+}
+
+func (s *taskStack) Len() int {
+	return len(s.tasks)
 }
