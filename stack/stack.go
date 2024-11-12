13c043e6b6e4fe4ef62e573497c234bcc509c3b2
marchuanv <marchuanv@gmail.com>
2024-11-13 01:27:35 +0200

    events

--- stack/stack.go
+++ stack/stack.go
@@ -8,42 +8,45 @@ import (
 type Stack[T any] struct {
 	stack  []T
 	events *StackEvent[T]
+	mu     sync.Mutex
 }
-
-var mu sync.Mutex
 
 func (s *Stack[T]) On(e Event) (T, T) {
 	s.initialise()
-	return s.events.subscribe(e)
+	return s.events.subscribe(e, false)
 }
 
 func (s *Stack[T]) OnAsync(e Event, f func(current T, previous T)) {
+	s.mu.Lock()
 	s.initialise()
+	s.mu.Unlock()
 	s.events.subscribeAsync(e, f)
 }
 
 func (s *Stack[T]) Len() int {
-	defer mu.Unlock()
-	mu.Lock()
-	return len(s.stack)
+	s.mu.Lock()
+	l := len(s.stack)
+	s.mu.Unlock()
+	return l
 }
 
 func (s *Stack[T]) Push(item T) {
-	defer mu.Unlock()
-	mu.Lock()
+	s.mu.Lock()
+	s.initialise()
 	l := len(s.stack)
 	var previous T
 	if l > 0 {
 		previous = s.stack[l-1]
 	}
 	s.stack = append(s.stack, item)
-	s.initialise()
 	s.events.raise(Push, item, previous)
+	s.mu.Unlock()
 }
 
 func (s *Stack[T]) Enqueue(item T) {
-	defer mu.Unlock()
-	mu.Lock()
+	defer s.mu.Unlock()
+	s.mu.Lock()
+	s.initialise()
 	var previous T
 	slices.Reverse(s.stack)
 	l := len(s.stack)
@@ -52,13 +55,13 @@ func (s *Stack[T]) Enqueue(item T) {
 	}
 	s.stack = append(s.stack, item)
 	slices.Reverse(s.stack)
-	s.initialise()
 	s.events.raise(Enqueue, item, previous)
 }
 
 func (s *Stack[T]) Pop() {
-	defer mu.Unlock()
-	mu.Lock()
+	defer s.mu.Unlock()
+	s.mu.Lock()
+	s.initialise()
 	l := len(s.stack)
 	var current T
 	var previous T
@@ -70,13 +73,12 @@ func (s *Stack[T]) Pop() {
 			current = s.stack[l-1]
 		}
 	}
-	s.initialise()
 	s.events.raise(Pop, current, previous)
 }
 
 func (s *Stack[T]) Peek() {
-	defer mu.Unlock()
-	mu.Lock()
+	s.mu.Lock()
+	s.initialise()
 	var current T
 	var previous T
 	l := len(s.stack)
@@ -86,13 +88,12 @@ func (s *Stack[T]) Peek() {
 			previous = s.stack[l-2]
 		}
 	}
-	s.initialise()
 	s.events.raise(Peek, current, previous)
+	s.mu.Unlock()
 }
 
 func (s *Stack[T]) initialise() {
 	if s.events == nil {
-		s.events = &StackEvent[T]{make(chan func() (Event, T, T)), false}
-		go s.events.initialise()
+		s.events = &StackEvent[T]{}
 	}
 }
--- stack/stackevent.go
+++ stack/stackevent.go
@@ -1,58 +1,71 @@
 package stack
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 const (
 	Push Event = iota
 	Pop
 	Enqueue
 	Peek
-	Health
 )
 
 type Event int
 
 type StackEvent[T any] struct {
-	events    chan func() (Event, T, T)
-	isPumping bool
+	stack                 map[Event][]func() (T, T)
+	mu                    sync.Mutex
+	invokeOnCreatedThread bool
 }
 
 func (se *StackEvent[T]) raise(e Event, current T, previous T) {
-	se.events <- func() (Event, T, T) {
-		return e, current, previous
-	}
+	funcStk := se.ensureReady(e)
+	se.mu.Lock()
+	funcStk = append(funcStk, func() (T, T) { return current, previous })
+	se.stack[e] = funcStk
+	se.mu.Unlock()
 }
 
-func (se *StackEvent[T]) subscribe(e Event) (T, T) {
-	var current, previous T
-	var event Event
-	for eventFunc := range se.events {
-		event, current, previous = eventFunc()
-		if event == e {
-			return current, previous
-		}
-		se.events <- eventFunc
+func (se *StackEvent[T]) subscribe(e Event, invokedByGoroutine bool) (T, T) {
+	funcStk := se.ensureReady(e)
+	se.mu.Lock()
+	if invokedByGoroutine && !se.invokeOnCreatedThread {
+		se.mu.Unlock()
+		time.Sleep(10 * time.Millisecond)
+		return se.subscribe(e, invokedByGoroutine)
 	}
-	return current, previous
+	if !invokedByGoroutine {
+		se.invokeOnCreatedThread = true
+	}
+	if len(funcStk) == 0 {
+		se.mu.Unlock()
+		time.Sleep(10 * time.Millisecond)
+		return se.subscribe(e, invokedByGoroutine)
+	}
+	eFunc := funcStk[len(funcStk)-1]
+	se.stack[e] = funcStk[:len(funcStk)-1]
+	se.mu.Unlock()
+	return eFunc()
 }
 
 func (se *StackEvent[T]) subscribeAsync(e Event, on func(current T, previous T)) {
 	go (func() {
-		current, previous := se.subscribe(e)
+		current, previous := se.subscribe(e, true)
 		on(current, previous)
 	})()
 }
 
-func (se *StackEvent[T]) initialise() {
-	if !se.isPumping {
-		se.isPumping = true
-		var current, previous T
-		se.events <- func() (Event, T, T) {
-			return Health, current, previous
-		}
-		for eFunc := range se.events {
-			time.Sleep(1 * time.Second)
-			se.events <- eFunc
-		}
+func (se *StackEvent[T]) ensureReady(e Event) []func() (T, T) {
+	se.mu.Lock()
+	if se.stack == nil {
+		se.stack = make(map[Event][]func() (T, T))
 	}
+	funcStk, exists := se.stack[e]
+	if !exists {
+		se.stack[e] = []func() (T, T){}
+	}
+	se.mu.Unlock()
+	return funcStk
 }
